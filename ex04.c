42cd287f2ffc51f5c719b42d580cccd2f5a72349
YusSuz <69309572+YusSuz@users.noreply.github.com>
2020-08-06 22:25:43 +0900

    最初のバージョン

---
+++ ex04.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(void){
+    // Your code here!
+    int i,num,heads=0,tails=0;
+    
+    for(i=0;i<3;i++){
+        num=rand();
+        if(num%2==0){
+            printf("Round%d is head\n",i+1);
+            heads++;
+        }else{
+            printf("Round%d is tail\n",i+1);
+            tails++;
+        }
+    }
+    printf("heads = %d  tails = %d\n",heads,tails);
+    return 0;
+}
