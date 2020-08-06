68c2b9a9efe53469ed9d58f4a8cf645c97771c84
YusSuz <69309572+YusSuz@users.noreply.github.com>
2020-08-06 23:17:33 +0900

    勝敗をつけました

--- ex04.c
+++ ex04.c
@@ -3,6 +3,17 @@ int main(void){
     // Your code here!
     int i,num,heads=0,tails=0;
     
+     int choice;
+    printf("Which is win do you think heads or tails?\n");
+    printf("Take 0 if you think heads will win, take 1 if you think tails will win.\n");
+    scanf("%d",&choice);
+    
+    if(choice == 0){
+        printf("You think heads will win.\n");
+    }else if(choice == 1){
+        printf("You think tails will win.\n");
+    }
+    
     for(i=0;i<3;i++){
         num=rand();
         if(num%2==0){
@@ -14,5 +25,20 @@ int main(void){
         }
     }
     printf("heads = %d  tails = %d\n",heads,tails);
+    
+    if(heads>tails){
+        if(choice == 0){
+            printf("You won!");
+        }else if(choice == 1){
+            printf("You lost...");
+            }
+    }
+    if(heads<tails){
+        if(choice == 1){
+            printf("You won!");
+        }else if(choice == 0){
+            printf("You lost...");
+        }
+    }
     return 0;
 }
